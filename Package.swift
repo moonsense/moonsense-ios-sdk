b0dad75f2da61c415d58a2dc085be3ffa174b09c
Ryan C. Payne <ryan@moonsense.io>
2021-12-06 11:23:07 -0500

    Update current Package.swift to point to Cloudsmith

--- Package.swift
+++ Package.swift
@@ -18,12 +18,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MoonsenseSDK",
-            url: "https://github.com/moonsense/moonsense-ios-sdk/releases/download/0.1.0-alpha5/MoonsenseSDK.xcframework.zip",
+            url: "https://dl.cloudsmith.io/basic/moonsense/sdk/raw/names/MoonsenseSDK.xcframework/versions/0.1.0-alpha5/MoonsenseSDK.xcframework.zip",
             checksum: "b728363ab9c45b7157afd2ce31005b764db26545cf676554adf412ac1d125918"
         ),
         .binaryTarget(
             name: "MoonsenseSDK-static",
-            url: "https://github.com/moonsense/moonsense-ios-sdk/releases/download/0.1.0-alpha5/MoonsenseSDK-static.xcframework.zip",
+            url: "https://dl.cloudsmith.io/basic/moonsense/sdk/raw/names/MoonsenseSDK-static.xcframework/versions/0.1.0-alpha5/MoonsenseSDK-static.xcframework.zip",
             checksum: "f0d5df48fa7fab1640273d67153e0a8f5c25d66c6c72a7eed5721fe7476e20f2"
         )
     ]
