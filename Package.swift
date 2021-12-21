c636a0870ced857199cfe20c495261d1005e2b41
Ryan C. Payne <ryan@moonsense.io>
2021-12-21 10:34:52 -0500

    Update download links in Package.swift

--- Package.swift
+++ Package.swift
@@ -18,12 +18,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MoonsenseSDK",
-            url: "https://dl.cloudsmith.io/basic/moonsense/sdk/raw/names/MoonsenseSDK.xcframework/versions/0.1.0/MoonsenseSDK.xcframework.zip",
+            url: "https://dl.moonsense.io/basic/moonsense/sdk/raw/names/MoonsenseSDK.xcframework/versions/0.1.0/MoonsenseSDK.xcframework.zip",
             checksum: "27386a63030c66ad286c7b22216c459682b67a74f5b8f4662424daa927d807b3"
         ),
         .binaryTarget(
             name: "MoonsenseSDK-static",
-            url: "https://dl.cloudsmith.io/basic/moonsense/sdk/raw/names/MoonsenseSDK-static.xcframework/versions/0.1.0/MoonsenseSDK-static.xcframework.zip",
+            url: "https://dl.moonsense.io/basic/moonsense/sdk/raw/names/MoonsenseSDK-static.xcframework/versions/0.1.0/MoonsenseSDK-static.xcframework.zip",
             checksum: "dc52aba710784bcb53eb2134e9b6ff2ee7ba494be2b5ee3ca6dc046606fd820d"
         )
     ]
